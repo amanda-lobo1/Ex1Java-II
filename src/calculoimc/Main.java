69be5f442c22ccfa991981d0733b4925d6844647
amanda-lobo1 <amanda.lobo@mercadolivre.com>
2022-09-23 21:08:36 -0300

    updated IMC exercise

--- src/calculoimc/Main.java
+++ src/calculoimc/Main.java
@@ -4,38 +4,42 @@ public class Main {
     public static void main(String[] args) {
         Pessoa p1 = new Pessoa();
         Pessoa p2 = new Pessoa("Amanda", "2", 20);
-        Pessoa p3 = new Pessoa("Daniel","3", 16, 50.5, 1.80);
+        Pessoa p3 = new Pessoa("Daniel","3", 16, 50.53, 1.75);
 
         if (p3.CalcularImc() == -1){
             if (p3.ehMaiorIdade()){
                 System.out.println(p3.getNome()+", maior de idade.");
-                System.out.println("Está abaixo do peso!");
+                System.out.println("Possui "+p3.getAltura()+" Metros e "+p3.getPeso()+"kg");
+                System.out.println(p3.getNome()+" está abaixo do peso!");
             }
             else {
                 System.out.println(p3.getNome()+", menor de idade.");
-                System.out.println("Está abaixo do peso!");
+                System.out.println("Possui "+p3.getAltura()+" Metros e "+p3.getPeso()+"kg");
+                System.out.println(p3.getNome()+" está abaixo do peso!");
             }
         }
 
         else if (p3.CalcularImc() == 0) {
-            if (p3.ehMaiorIdade()){
-                System.out.println(p3.getNome()+", maior de idade.");
-                System.out.println("Você está com um peso saudável!");
-            }
-            else {
-                System.out.println(p3.getNome()+", menor de idade.");
-                System.out.println("Você está com um peso saudável!");
+            if (p3.ehMaiorIdade()) {
+                System.out.println(p3.getNome() + ", maior de idade.");
+                System.out.println("Possui " + p3.getAltura() + " Metros e " + p3.getPeso() + "kg");
+                System.out.println(p3.getNome() + " está em um peso saudável!");
+            } else {
+                System.out.println(p3.getNome() + ", menor de idade.");
+                System.out.println("Possui " + p3.getAltura() + " Metros e " + p3.getPeso() + "kg");
+                System.out.println(p3.getNome() + " está em um peso saudável!");
             }
         }
 
         else if (p3.CalcularImc() == 1){
-            if (p3.ehMaiorIdade()){
-                System.out.println(p3.getNome()+", maior de idade.");
-                System.out.println("Você está acima do peso! ");
-            }
-            else {
+            if (p3.ehMaiorIdade()) {
+                System.out.println(p3.getNome() + ", maior de idade.");
+                System.out.println("Possui " + p3.getAltura() + " Metros e " + p3.getPeso() + "kg");
+                System.out.println(p3.getNome() + " está acima do peso!");
+            } else {
                 System.out.println(p3.getNome() + ", menor de idade.");
-                System.out.println("Você está acima do peso! ");
+                System.out.println("Possui " + p3.getAltura() + " Metros e " + p3.getPeso() + "kg");
+                System.out.println(p3.getNome() + " está acima do peso!");
             }
         }
 
